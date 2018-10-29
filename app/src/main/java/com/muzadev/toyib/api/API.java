eff0c5f2878545f6c27712832f7f5cb99580a81b
Zulfakar Muhammad <muhammad.zulfakar@students.amikom.ac.id>
2018-10-29 22:09:57 +0700

    update API
    tambah sedikit penjelasan

--- app/src/main/java/com/muzadev/toyib/api/API.java
+++ app/src/main/java/com/muzadev/toyib/api/API.java
@@ -5,6 +5,7 @@ package com.muzadev.toyib.api;
  * For educational purposes
  */
 public class API {
+//  ini API_KEY ku, nanti tinggal diganti pake API_KEY njenengan masing-masing :D
     private static final String API_KEY = "280af572647b3e8153b144e478ba7a36";
     public static final String NOW_PLAYING = "https://api.themoviedb.org/3/movie/";
     public static final String POSTER = "http://image.tmdb.org/t/p/w185/";
